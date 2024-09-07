d32acb09aecb8f2942074bd24e81bc0e3ceaaea8
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2024-09-07 22:04:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,31 +13,34 @@ private:
             next = nullptr;
         }
     };
-
+    
     Node* f;
     Node* l;
-
+    
 public:
-
+    
     MyList(){
         f = nullptr;
         l = nullptr;
     }
-
+    
     int length(){
-        if(f==nullptr)
+        if(f == nullptr)
             return 0;
+        
         Node* node = f;
         int c = 0;
-        while(node!=nullptr){
+        while(node != nullptr){
             node = node->next;
             c++;
         }
+        
         return c;
     }
-
+    
     void push_back(T value){
         Node* new_node = new Node(value);
+        
         if(f == nullptr){
             f = new_node;
             l = new_node;
@@ -47,75 +50,89 @@ public:
             l = new_node;
         }
     }
-
-    Node* find(T v){
-        if(f!=nullptr){
+    
+    int find(T v){
+        if(f != nullptr){
             Node* node = f;
-            while(node!=nullptr){
+            int c = 0;
+            
+            while(node != nullptr){
                 if(node->value == v)
-                    return node;
+                    return c;
                 node = node->next;
+                c++;
             }
         }
-        return nullptr;
+        cout<<"Nothing.\n";
     }
-
+    
     void pop_front(){
-        if(f!=nullptr){
-            Node* node = f;
-            f = node->next;
-            delete node;
-        }
+        if(f == nullptr)
+            return;
+        Node* node = f;
+        f = node->next;
+        delete node;
     }
-
+    
     void pop_back(){
-        if(f!=nullptr){
-            if(f==l)
-                pop_front();
-            else{
-                Node* node = f;
-                while(node->next!=l)
-                    node = node->next;
-                node->next = nullptr;
-                delete l;
-                l = node;
-            }
+        if(f == nullptr)
+            return;
+        
+        if(f == l)
+            pop_front();
+        
+        else{
+            Node* node = f;
+            
+            while(node->next!=l)
+                node = node->next;
+            
+            node->next = nullptr;
+            delete l;
+            l = node;
         }
     }
-
+    
     Node* operator[](int index){
         int c = 0;
         Node* node = f;
-        while(c!=index and node!=nullptr){
+        
+        while(c != index and node != nullptr){
             node = node->next;
             c++;
         }
-        if(node==nullptr)
+        
+        if(node == nullptr)
             cout<<"Index out of range!";
         return node;
     }
-
+    
     void clear(){
-        while(f!=nullptr){
+        while(f != nullptr){
             Node* node = f;
             f = node->next;
             delete node;
         }
     }
-
+    
     void insert(int index, T value){
+        if(index >= length() or index < 0)
+            return;
+        
         Node* new_node = new Node(value);
         Node* node = f;
         int c = 0;
-        while (c<index) {
+        
+        while (c < index) {
             node = f->next;
             c++;
         }
+        
         Node* _node = node->next;
         new_node->next = _node;
         node->next = new_node;
     }
-
+    
     void push_front(T value){
         Node* new_node = new Node(value);
         new_node->next = f;
@@ -124,37 +141,40 @@ public:
 private:
     friend ostream& operator << (ostream &os, const MyList<T> &q){
         Node* node = q.f;
-        while (node!=nullptr) {
+        while (node != nullptr) {
             os<<node->value<<" ";
             node = node->next;
         }
         return os;
     }
-
+    
     friend ostream& operator << (ostream &os, Node* node){
         return os << node->value;
     }
-
+    
     friend istream& operator >> (istream &in, MyList<T> &q){
         T value;
         in>>value;
         q.push_back(value);
         return in;
-
+        
     }
-
+    
 };
 
 int main()
 {
     MyList<int> a;
-
+    
     a.push_back(1);
     a.push_back(2);
     a.push_back(3);
     a.push_back(4);
     a.push_front(6);
     a.insert(1,12);
+    a.pop_front();
+    a.pop_back();
+    a.clear();
     cout<<a;
     a.clear();
     a.push_back(1);
@@ -162,6 +182,6 @@ int main()
     MyList<int> e;
     cin>>e;
     cout<<e;
-
+    
     return 0;
 }
