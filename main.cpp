835055409f870ba7887e26ae4d80b8a4231e7a5a
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2024-10-05 15:12:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,16 @@ class MyList{
 private:
     struct Node{
         T* value;
+        T val;
         Node* next;
-        Node(T *_value){
+        Node(T* _value){
             value = _value;
             next = nullptr;
+        }
+        Node(T _value){
+            val = _value;
+            next = nullptr;
+            value = &val;
         }
     };
 
@@ -20,6 +26,15 @@ private:
     int length = 0;
 
 public:
+
+    MyList& operator = (const MyList& other){
+        if(this != &other){
+            f = other.f;
+            l = other.l;
+            length = other.length;
+        }
+        return *this;
+    }
 
     int getLength(){
         return length;
@@ -45,7 +60,22 @@ public:
         length++;
     }
 
-    int find(T v){
+    void push_back(T value){
+        Node* new_node = new Node(value);
+
+        if(f == nullptr){
+            f = new_node;
+            l = new_node;
+        }
+        else{
+            l->next = new_node;
+            l = new_node;
+        }
+
+        length++;
+    }
+
+    int find(T* v){
         if(f != nullptr){
             Node* node = f;
             int c = 0;
@@ -58,7 +88,26 @@ public:
             }
         }
         cout<<"Nothing.\n";
+        return -1;
     }
+
+
+    int find(T v){
+        if(f != nullptr){
+            Node* node = f;
+            int c = 0;
+
+            while(node != nullptr){
+                if(*node->value == v)
+                    return c;
+                node = node->next;
+                c++;
+            }
+        }
+        cout<<"Nothing.\n";
+        return -1;
+    }
+
 
     void delete_element(T* v){
         if(f == nullptr)
@@ -75,6 +124,43 @@ public:
                 break;
             }
             if(node->next->value == v){
+                c = 1;
+                break;
+            }
+            node = node->next;
+        }
+
+        if(!c)
+            return;
+
+        if(node != nullptr){
+            c = 1;
+            Node* _node = node->next;
+            node->next = node->next->next;
+            delete _node;
+        }
+
+        length--;
+
+        if(c)
+            delete_element(v);
+    }
+
+    void delete_element(T v){
+        if(f == nullptr)
+            return;
+
+        if(*f->value == v)
+            pop_front();
+
+        bool c = 0;
+        Node* node = f;
+
+        while(node != nullptr){
+            if(node->next == nullptr){
+                break;
+            }
+            if(*node->next->value == v){
                 c = 1;
                 break;
             }
@@ -151,10 +237,31 @@ public:
         if(index >= length or index < 0)
             return;
 
-        if(index == 0){
-            push_front(value);
+
+        if(index == length-1){
+            push_back(value);
             return;
         }
+
+        Node* new_node = new Node(value);
+        Node* node = f;
+        int c = 0;
+
+        while (c < index) {
+            node = f->next;
+            c++;
+        }
+
+        Node* _node = node->next;
+        new_node->next = _node;
+        node->next = new_node;
+
+        length++;
+    }
+
+    void insert(int index, T* value){
+        if(index >= length or index < 0)
+            return;
 
         if(index == length-1){
             push_back(value);
@@ -184,6 +291,23 @@ public:
 
         length++;
     }
+
+    void push_front(T* value){
+        Node* new_node = new Node(value);
+        new_node->next = f;
+        f = new_node;
+
+        length++;
+    }
+
+    void copy(MyList b){
+        if(this != &b)
+            return;
+        clear();
+        for(int i = 0; i < b.length; i++)
+            push_back(b[i]);
+    }
+
 private:
     friend ostream& operator << (ostream &os, const MyList<T> &q){
         Node* node = q.f;
@@ -210,40 +334,34 @@ private:
 
 int main()
 {
-    MyList<MyList<int>> a;
-
-    MyList<int> b;
     MyList<int> c;
-    MyList<int> d;
-    MyList<int> e;
-
-    int b1 = 3;
-    int c1 = 4;
-    int d1 = 5;
-    int e1 = 6;
-
-    b.push_back(&b1);
-    c.push_back(&c1);
-    c.push_back(&b1);
-    c.push_back(&e1);
-    c.push_back(&c1);
-    c.push_back(&d1);
-    d.push_back(&d1);
-    e.push_back(&e1);
-
-    a.push_back(&b);
+    c.push_back(1);
+    c.push_back(2);
+    int b = 123;
+    c.insert(1,&b);
+    int e = 321;
+    int h = 44;
+    c.push_front(e);
+    c.push_front(&h);
+    cout<<c<<"\n";
+    c.delete_element(1);
+    cout<<c<<"| "<<c.find(&h)<<" "<<c.find(2)<<"\n";
+    cin>>c;
+    cout<<c<<"\n";
+    MyList<MyList<int>> a;
     a.push_back(&c);
-    a.push_back(&d);
-    a.push_back(&e);
-
-    a.pop_back();
-
-    cout<<a.getLength()<<endl;
-
-    a.delete_element(&b);
-
-    cout<<a.getLength()<<endl;
-
-    cout<<a[0][1];
+    MyList<int> q;
+    q.push_back(12);
+    q.push_back(5);
+    a.push_back(&q);
+    cout<<a<<"\n";
+    c.copy(q);
+    cout<<a<<"\n";
+    q[0] = 1;
+    cout<<a<<"\n";
+    MyList<MyList<int>> a1 = a;
+    cout<<"\na1 = "<<a1<<"\n7";
+    a[0][1] = 888;
+    cout<<"\na1 = "<<a1;
     return 0;
 }
